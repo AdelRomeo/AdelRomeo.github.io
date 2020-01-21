7d3bc4d1f3c7b951244c265b359d077738a32b5a
AdelRomeo <11cage11@mail.ru>
2020-01-21 21:52:55 +0700

    particles v2

--- src/scripts/mainparticl.js
+++ src/scripts/mainparticl.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function () {
     /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-    particlesJS.load('particl', '/scripts/particles.json', function() {
+    particlesJS.load('particl', 'src/scripts/particles.json', function() { // путь относительно index.html
         console.log('callback - particles.js config loaded');
     });
 });

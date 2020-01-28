c18b97c5fad941e41f831a9bcadd49ae1a201986
AdelRomeo <11cage11@mail.ru>
2020-01-28 22:49:15 +0700

    VK api test

--- src/scripts/vkApi.js
+++ src/scripts/vkApi.js
@@ -1,3 +1,11 @@
-const Handlebars = require(["handlebars"]);
-const template = Handlebars.compile("Name: {{name}}");
-console.log(template({ name: "Nils21" }));
+VK.init({ // подключение приложения
+    apiId: 7298671 // id приложения
+});
+
+VK.Auth.login((data)=>{ // отправка запроса на разрешение
+    if (data.session) { // если разрешил
+        console.log('все ок')
+    } else {
+        console.log(new Error('Что то пшло не так'))
+    }
+}, 8);

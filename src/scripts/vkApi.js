8efec854c0135d0ec0ba109c3f73a24dac25e12f
AdelRomeo <11cage11@mail.ru>
2020-01-28 21:13:24 +0700

    handlebars test

--- src/scripts/vkApi.js
+++ src/scripts/vkApi.js
@@ -1,11 +1,3 @@
-VK.init({ // инициализация запроса
-    apiId: 7292575 // id от какого приложения запрос
-});
-
-VK.Auth.login(data =>{
-    if (data.session){
-        console.log('ok')
-    } else {
-        console.log(new Error('Не удалось'))
-    }
-}, 2); // 2 указывает к чему хотим получить доступ
+const Handlebars = require("handlebars");
+const template = Handlebars.compile("Name: {{name}}");
+console.log(template({ name: "Nils21" }));

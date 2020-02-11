bf81713f5a894a4862354f2896c3e3bc39dcfc06
AdelRomeo <11cage11@mail.ru>
2020-02-11 22:56:24 +0700

    new index page v2

--- src/scripts/indexScript.js
+++ src/scripts/indexScript.js
@@ -1,13 +1,26 @@
 let btnS = document.querySelectorAll('.main-menu > button');
 const menu = document.querySelector('.main-menu');
 const backEffect = document.querySelector('.hover-affect');
+const body = document.querySelector('body');
 
 (() => {
     menu.addEventListener('click', async (e)=>{
-        if (e.target !== btnS[0]){
-            backEffect.classList.add('hover-affect-hover')
-        } else {
-            backEffect.classList.remove('hover-affect-hover')
+
+        // if (e.target !== btnS[0]){ // кружок рядом с фоном
+        //     backEffect.classList.add('hover-affect-hover')
+        // }  else {
+        //     backEffect.classList.remove('hover-affect-hover')
+        // }
+
+        for (let i = 1; i < btnS.length; i++){
+            if (e.target === btnS[i]){
+                menu.style.marginLeft = '00px';
+                menu.style.width = '400px';
+            }
+            if (e.target === btnS[0]){
+                menu.style.marginLeft = '0';
+                menu.style.width = '100%';
+            }
         }
 
 
@@ -18,7 +31,6 @@ const backEffect = document.querySelector('.hover-affect');
         if (e.target === btnS[1]){
             backEffect.style.top = '120px';
             backEffect.style.borderRadius = '50px';
-            backEffect.classList.add('shake');
         }
         if (e.target === btnS[2]){
             backEffect.style.top = '240px';
@@ -32,5 +44,6 @@ const backEffect = document.querySelector('.hover-affect');
             backEffect.style.top = '480px';
             backEffect.style.borderRadius = '50px';
         }
+
     })
 })();

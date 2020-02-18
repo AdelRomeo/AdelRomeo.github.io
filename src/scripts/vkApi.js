9ac516db44e2c7fea177a9564e26716f41379977
AdelRomeo <11cage11@mail.ru>
2020-02-18 18:17:46 +0700

    yMap api v4

--- src/scripts/vkApi.js
+++ src/scripts/vkApi.js
@@ -34,8 +34,8 @@ auth()
     .then((data)=>{ // когда список друзей будет получен
         const resu = document.querySelector('.results');
         for (let i = 0; i < data.items.length; i++){ // перебор всех друзей
-            resu.innerHTML += data.items[i].id + ' ' + data.items[i].last_name + '<br>';
-           // resu.innerHTML += data.items[i].country.title + data.items[i].city.title + '<br>';
+           // resu.innerHTML += data.items[i].id + ' ' + data.items[i].last_name + '<br>';
+            resu.innerHTML += data.items[i].country + '<br>';
             console.log(data.items[i].id);
         }
         console.log(data);
